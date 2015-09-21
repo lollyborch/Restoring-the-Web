896cba7c22e0bf869b0e289003bfc75fdd5493ca
Felix <felixlin21@gmail.com>
2015-09-21 13:15:56 +1000

    Some name changes

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
       $editorControlButtons = $('.control-buttons'),
       aceEditor,
       resizable = true,
-      testMarkup = '<h1>News Item Title</h2>\n<section>\nPop-up artisan reprehenderit single-origin coffee. Wolf voluptate pickled, lomo Bushwick non labore. Neutra selvage readymade tote bag, irure consectetur gluten-free aliquip fugiat pop-up. Vice eu vero anim, cray McSweeneys you probably havent heard of them pickled. Est small batch bespoke drinking vinegar. Direct trade delectus mustache, elit viral ennui master cleanse literally nostrud pickled street art swag small batch dolore mlkshk. Try-hard disrupt sapiente tattooed master cleanse.\n</section>\n<br />\n<section>\nAdipisicing anim PBR&B, mixtape American Apparel voluptate VHS kale chips jean shorts. Leggings qui magna shabby chic, swag laborum 3 wolf moon semiotics ethical brunch ad Neutra. Kale chips freegan letterpress dolor ea. Marfa single-origin coffee blog cillum Intelligentsia. Ex High Life ea, hashtag dolor ethnic fingerstache Portland +1 gastropub cred quis Blue Bottle Tumblr. Ethnic VHS et food truck consequat, PBR&B Tonx brunch cred freegan fap. Typewriter Helvetica church-key, small batch id pop-up hashtag readymade actually cliche kogi sapiente synth ethical.\n</section>';
+      testMarkup = '<p>Website code here!</p>';
   
   
   function setupEditor () {
