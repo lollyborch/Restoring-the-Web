51b39ba62736b7319625b99b372d5f069dfaf399
lollyborch <lilly.borchardt@gmail.com>
2015-10-14 13:29:27 +1000

    tabs working again

--- js/WebsiteController.js
+++ js/WebsiteController.js
@@ -1,57 +1,90 @@
-//var app = angular.module("myApp", []);
-//var imageArraySport = ['http://www.placecage.com/250/150', 'http://www.fillmurray.com/g/250/150', 'https://placekitten.com/250/150' ];
+var app = angular.module("myApp", []);
+var imageArray = ["http://nla.gov.au/nla.obj-148694544/representativeImage", "http://nla.gov.au/nla.obj-136587996/representativeImage", "http://nla.gov.au/nla.obj-150937117/representativeImage"];
 
 app.controller('WebsiteController', ['$scope', function ($scope) {
   $scope.websiteSport = [
         {
-            title: 'Cricket Australia'
+            title: 'Cricket Australia',
+            imgalt: 'Cricket Australia',
+            url: 'cricketaustralia.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         },
         {
-            title: 'Sydney Olympics'
+            title: 'Sydney Olympics',
+            imgalt: 'Sydney Olympics',
+            url: 'sydneyolympics.html',
+            level: 'images/Hard.png',
+            levelalt: 'hard level'
         },
         {
-            title: 'Cycling Queensland'
+            title: 'Cycling Queensland',
+            imgalt: 'Cycling Queensland',
+            url: 'cyclingqueensland.html',
+            level: 'images/Hard.png',
+            levelalt: 'hard level'
         }
     ];
-    //adds images from the Trove API call array 'imageArraySport' to the above $scope.websiteSport - change imageArray name when sort out js load
-    var sportIndex = 0
+    
+   /* var sportIndex = 0
     $scope.websiteSport.forEach(function (newImage, sportIndex) {
-    newImage.coverImg = imageArraySport[sportIndex];
-    });
+    newImage.coverImg = imageArray[sportIndex];
+});*/
     
     $scope.websiteArts = [
         
         {
             title: 'Queensland Ballet',
-            coverImg: 'http://www.fillmurray.com/g/250/150'
+            imgalt: 'Queensland Ballet',
+            url: 'queenslandballet.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         },
         {
             title: 'Brisbane Writers Festival',
-            coverImg: 'http://www.fillmurray.com/g/250/150'
+            imgalt: 'Brisbane Writers Festival',
+            url: 'brisbanewritersfestival.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         }
     ];
     $scope.websiteEdu = [
         {
             title: 'UQ',
-            coverImg: 'http://www.fillmurray.com/250/150'
+            imgalt: 'UQ',
+            url: 'uq.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         },
         {
             title: 'QTAC',
-            coverImg: 'http://www.fillmurray.com/250/150'
+            imgalt: 'QTAC',
+            url: 'qtac.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         }
     ];
     $scope.websiteGov = [
         {
             title: 'Brisbane City Council',
-            coverImg: 'http://www.placekitten.com/g/250/150'
+            imgalt: 'Brisbane City Council',
+            url: 'brisbanecitycouncil.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         },
         {
             title: 'Qld Art Gallery',
-            coverImg: 'http://www.placekitten.com/g/250/150'
+            imgalt: 'Qld Art Gallery',
+            url: 'qldartgallery.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         },
         {
             title: 'Qld Museum',
-            coverImg: 'http://www.placekitten.com/g/250/150'
+            imgalt: 'Qld Museum',
+            url: 'qldmuseum.html',
+            level: 'images/Easy.png',
+            levelalt: 'easy level'
         }
     ];
   
