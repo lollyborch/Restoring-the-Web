41f51cb9fe9dca4e204a90ca5d0ccd1eaf8c3ecb
lollyborch <lilly.borchardt@gmail.com>
2015-10-14 22:14:45 +1000

    updated api calls and styles

--- js/trove.js
+++ js/trove.js
@@ -22,7 +22,7 @@ $(document).ready(function cricket() {
 					var cricketImg = cricketImgLink + "/representativeImage"
                     imageArraySport[0]=cricketImg;
 					//imageArraySport.push(cricketImg);
-					console.log(imageArraySport);
+					//console.log(imageArraySport);
 					printSportImg();
 					break;
 				}	
@@ -48,7 +48,7 @@ $(document).ready(function olympics() {
 					var olympicsImg = olympicsImgLink + "/representativeImage"
                     imageArraySport[1]=olympicsImg;
 					//imageArraySport.push(olympicsImg);
-					console.log(imageArraySport);
+					//console.log(imageArraySport);
 					printSportImg();
 					break;
 				}	
@@ -73,7 +73,7 @@ $(document).ready(function cycling() {
 					var cyclingImg = cyclingImgLink + "/representativeImage"
                     imageArraySport[2]=cyclingImg;
 					//imageArraySport.push(cyclingImg);
-					console.log(imageArraySport);
+					//console.log(imageArraySport);
 					printSportImg();
 					break;
 				}	
@@ -87,10 +87,229 @@ function printSportImg() {
     if(imageArraySport.length == 3){
         $('.sportimage').each(function(index) {
             $(this).attr('src', imageArraySport[index]);
-            console.log(imageArraySport[index]);
+            //console.log(imageArraySport[index]);
         });
     };
 
 };
 
-//printSportImg();
+var imageArrayArts = [];
+
+//ballet image
+$(document).ready(function ballet() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=ballet&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 37280055) {
+					var balletImgLink = recordList[i].identifier[0].value;
+					var balletImg = balletImgLink + "/representativeImage"
+                    imageArrayArts[0]=balletImg;
+					//console.log(imageArrayArts);
+                    printArtsImg();
+                  //  console.log("I have reached printArtsImg ballet");
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+//writers fest image
+$(document).ready(function writers() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=paper&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 33077574) {
+					var writersImgLink = recordList[i].identifier[0].value;
+					var writersImg = writersImgLink + "/representativeImage"
+                    imageArrayArts[1]=writersImg;
+					//console.log(imageArrayArts);
+                    printArtsImg();
+                  //  console.log("I have reached printArtsImg writers");
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+function printArtsImg() {
+    if(imageArrayArts.length == 2){
+       // console.log("the array length is 2");
+        $('.artimage').each(function(index) {
+            $(this).attr('src', imageArrayArts[index]);
+          //  console.log(imageArrayArts[index]);
+        });
+    };
+    
+};
+
+var imageArrayEducation = [];
+
+//UQ image
+$(document).ready(function UQ() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=UQ+building&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 191089369) {
+					var UQImgLink = recordList[i].identifier[0].value;
+                    imageArrayEducation[0]=UQImgLink;
+					console.log(imageArrayEducation);
+                    printEducationImg();
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+//QTAC study image
+$(document).ready(function qtac() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=UQ+library+study&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 191093925) {
+					var studyImgLink = recordList[i].identifier[0].value;
+					var studyImg = studyImgLink + "/web_n1959_S909_p110a.jpg"
+                    imageArrayEducation[1]=studyImg;
+					console.log(imageArrayEducation);
+                    printEducationImg();
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+function printEducationImg() {
+    if(imageArrayEducation.length == 2){
+        console.log("the array length is 2");
+        $('.educationimage').each(function(index) {
+            $(this).attr('src', imageArrayEducation[index]);
+            console.log(imageArrayEducation[index]);
+        });
+    };
+    
+};
+
+var imageArrayGovernment = [];
+
+//BCC image
+$(document).ready(function BCC() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=brisbane+council+espace&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 191090820) {
+					var BCCImgLink = recordList[i].identifier[0].value;
+                    imageArrayGovernment[0]=BCCImgLink;
+					console.log(imageArrayGovernment);
+                    printGovernmentImg();
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+//QAG study image
+$(document).ready(function qag() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=queensland+art+espace&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 191093713) {
+					var artImgLink = recordList[i].identifier[0].value;
+					var artImg = artImgLink + "/web_Slide_008.jpg"
+                    imageArrayGovernment[1]=artImg;
+					console.log(imageArrayGovernment);
+                    printGovernmentImg();
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+//Qld museum image
+$(document).ready(function qldmuseum() {
+	var url = "http://api.trove.nla.gov.au/result?key=jsk1qqntnrj7qbvf&zone=picture&l-availability=y&q=queensland+museum+espace&encoding=json&n=50"
+	$.ajax({
+		type: "GET",
+		url: url,
+		asynch: false,
+		contentType: "application/json",
+		dataType: "jsonp",
+		success: function(callData) {
+		var recordList = callData.response.zone[0].records.work;
+			for (i = 0; i < recordList.length; i++) {
+				if (recordList[i].id == 191093709) {
+					var museumImgLink = recordList[i].identifier[0].value;
+					var museumImg = museumImgLink + "/web_Slide_020.jpg"
+                    imageArrayGovernment[2]=museumImg;
+					console.log(imageArrayGovernment);
+                    printGovernmentImg();
+					break;
+				}	
+			}
+		}
+	});
+    
+});
+
+function printGovernmentImg() {
+    if(imageArrayGovernment.length == 3){
+        console.log("the array length is 2");
+        $('.governmentimage').each(function(index) {
+            $(this).attr('src', imageArrayGovernment[index]);
+            console.log(imageArrayGovernment[index]);
+        });
+    };
+    
+};
