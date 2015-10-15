b82f9d514b9b1e58908fe0c64d89878a938f927d
lollyborch <lilly.borchardt@gmail.com>
2015-10-15 18:32:21 +1000

    moved old code and files

--- lillytest/Exercise Files/03_01/finished/angular/js/controllers.js
+++
@@ -1,9 +0,0 @@
-var artistControllers = angular.module('artistControllers', []);
-
-artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
-    $scope.artistOrder = 'name';
-  });
-}]);
-
