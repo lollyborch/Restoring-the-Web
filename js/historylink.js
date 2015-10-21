187bfeec2a8ea6cea6401d73f3e9f565c0f46f4e
lollyborch <lilly.borchardt@gmail.com>
2015-10-21 12:12:53 +1000

    history link js

---
+++ js/historylink.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+    if(WebsiteID==1){
+      $('.class or #id').attr('src', 'link');
+        console.log("website id 1 link")
+        });
+  }else {
+    console.log("no website 1")
+  }
+
+});
