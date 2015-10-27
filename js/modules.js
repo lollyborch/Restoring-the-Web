cec839c32083ca49a171184d490ef0e1e84faaf3
lollyborch <lilly.borchardt@gmail.com>
2015-10-27 13:03:35 +1000

    dynamic modules for all

--- js/modules.js
+++ js/modules.js
@@ -75,3 +75,254 @@ $(document).ready( function() {
     console.log("I got nothing")
   }
 });
+
+function cricketmoduleclass() {
+    console.log("i have reached the cricketmoduleclass");
+    if(cricketmod1==1){
+      console.log("cricket mod 1 JS = " + cricketmod1);
+      $('#modCA1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod2==1){
+      console.log("cricket mod 2 JS = " + cricketmod2);
+      $('#modCA2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod3==1){
+      console.log("cricket mod 3 JS = " + cricketmod3);
+      $('#modCA3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod4==1){
+      console.log("cricket mod 4 JS = " + cricketmod4);
+      $('#modCA4').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod5==1){
+      console.log("cricket mod 5 JS = " + cricketmod5);
+      $('#modCA5').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod6==1){
+      console.log("cricket mod 6 JS = " + cricketmod6);
+      $('#modCA6').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod7==1){
+      console.log("cricket mod 7 JS = " + cricketmod7);
+      $('#modCA7').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cricketmod8==1){
+      console.log("cricket mod 8 JS = " + cricketmod8);
+      $('#modCA8').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+
+};
+
+function olympicsmoduleclass() {
+    console.log("i have reached the olympicsmoduleclass");
+    if(olympicmod1==1){
+      console.log("olympics mod 1 JS = " + olympicmod1);
+      $('#modSO1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(olympicmod2==1){
+      console.log("olympics mod 2 JS = " + olympicmod2);
+      $('#modSO2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(olympicmod3==1){
+      console.log("olympics mod 3 JS = " + olympicmod3);
+      $('#modSO3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(olympicmod4==1){
+      console.log("olympics mod 4 JS = " + olympicmod4);
+      $('#modSO4').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(olympicmod5==1){
+      console.log("olympics mod 5 JS = " + olympicmod5);
+      $('#modSO5').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(olympicmod6==1){
+      console.log("olympics mod 6 JS = " + olympicmod6);
+      $('#modSO6').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function gallerymoduleclass() {
+    console.log("i have reached the gallerymoduleclass");
+    if(gallerymod1==1){
+      console.log("gallery mod 1 JS = " + gallerymod1);
+      $('#modQAG1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(gallerymod2==1){
+      console.log("gallery mod 2 JS = " + gallerymod2);
+      $('#modQAG2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(gallerymod3==1){
+      console.log("gallery mod 3 JS = " + gallerymod3);
+      $('#modQAG3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(gallerymod4==1){
+      console.log("gallery mod 4 JS = " + gallerymod4);
+      $('#modQAG4').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(gallerymod5==1){
+      console.log("gallery mod 5 JS = " + gallerymod5);
+      $('#modQAG5').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function councilmoduleclass() {
+    console.log("i have reached the councilmoduleclass");
+    if(councilmod1==1){
+      console.log("council mod 1 JS = " + councilmod1);
+      $('#modBCC1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(councilmod2==1){
+      console.log("council mod 2 JS = " + councilmod2);
+      $('#modBCC2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(councilmod3==1){
+      console.log("council mod 3 JS = " + councilmod3);
+      $('#modBCC3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function uqmoduleclass() {
+    console.log("i have reached the uqmoduleclass");
+    if(uqmod1==1){
+      console.log("uq mod 1 JS = " + uqmod1);
+      $('#modUQ1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(uqmod2==1){
+      console.log("uq mod 2 JS = " + uqmod2);
+      $('#modUQ2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(uqmod3==1){
+      console.log("uq mod 3 JS = " + uqmod3);
+      $('#modUQ3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function balletmoduleclass() {
+    console.log("i have reached the balletmoduleclass");
+    if(balletmod1==1){
+      console.log("ballet mod 1 JS = " + balletmod1);
+      $('#modQB1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(balletmod2==1){
+      console.log("ballet mod 2 JS = " + balletmod2);
+      $('#modQB2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(balletmod3==1){
+      console.log("ballet mod 3 JS = " + balletmod3);
+      $('#modQB3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function cyclingmoduleclass() {
+    console.log("i have reached the cyclingmoduleclass");
+    if(cyclingmod1==1){
+      console.log("cycling mod 1 JS = " + cyclingmod1);
+      $('#modCQ1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cyclingmod2==1){
+      console.log("cycling mod 2 JS = " + cyclingmod2);
+      $('#modCQ2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(cyclingmod3==1){
+      console.log("cycling mod 3 JS = " + cyclingmod3);
+      $('#modCQ3').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function qtacmoduleclass() {
+    console.log("i have reached the qtacmoduleclass");
+    if(qtacmod1==1){
+      console.log("qtac mod 1 JS = " + qtacmod1);
+      $('#modQTAC1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+    if(qtacmod2==1){
+      console.log("qtac mod 2 JS = " + qtacmod2);
+      $('#modQTAC2').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function writersmoduleclass() {
+    console.log("i have reached the writersmoduleclass");
+    if(writersmod1==1){
+      console.log("writers mod 1 JS = " + writersmod1);
+      $('#modBWF1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
+
+function museummoduleclass() {
+    console.log("i have reached the museummoduleclass");
+    if(museummod1==1){
+      console.log("museum mod 1 JS = " + museummod1);
+      $('#modQM1').addClass('moduleboxStart').removeClass('modulebox');
+      }else {
+        console.log("I got nothing for mod JS")
+      }
+};
--- my_restorations.php
+++ my_restorations.php
@@ -25,7 +25,7 @@ require("base.php");
 	$uHTML = $_SESSION['userHTML'];
     
     //Hard coded User ID
-    //$currentUserID1 = 19;
+    $currentUserID1 = 19;
 
     /* variables that query the mySQL database to see if there is an entry for a particular module */
     $cricket_start_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND UserID = $currentUserID ");
@@ -51,6 +51,106 @@ require("base.php");
     $council_start = mysql_num_rows($council_start_mysql);
     $gallery_start = mysql_num_rows($gallery_start_mysql);
     $museum_start = mysql_num_rows($museum_start_mysql);
+
+    //cricket modules
+    $cricket_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '1' AND UserID = $currentUserID ");
+    $cricket_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '2' AND UserID = $currentUserID ");
+    $cricket_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '3' AND UserID = $currentUserID ");
+    $cricket_mod4_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '4' AND UserID = $currentUserID ");
+    $cricket_mod5_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '5' AND UserID = $currentUserID ");
+    $cricket_mod6_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '6' AND UserID = $currentUserID ");
+    $cricket_mod7_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '7' AND UserID = $currentUserID ");
+    $cricket_mod8_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '1' AND CompID = '8' AND UserID = $currentUserID ");
+
+    $cricket_mod1_start = mysql_num_rows($cricket_mod1_mysql);
+    $cricket_mod2_start = mysql_num_rows($cricket_mod2_mysql);
+    $cricket_mod3_start = mysql_num_rows($cricket_mod3_mysql);
+    $cricket_mod4_start = mysql_num_rows($cricket_mod4_mysql);
+    $cricket_mod5_start = mysql_num_rows($cricket_mod5_mysql);
+    $cricket_mod6_start = mysql_num_rows($cricket_mod6_mysql);
+    $cricket_mod7_start = mysql_num_rows($cricket_mod7_mysql);
+    $cricket_mod8_start = mysql_num_rows($cricket_mod8_mysql);
+
+    //gallery modules
+    $gallery_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '10' AND CompID = '1' AND UserID = $currentUserID ");
+    $gallery_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '10' AND CompID = '2' AND UserID = $currentUserID ");
+    $gallery_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '10' AND CompID = '3' AND UserID = $currentUserID ");
+    $gallery_mod4_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '10' AND CompID = '4' AND UserID = $currentUserID ");
+    $gallery_mod5_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '10' AND CompID = '5' AND UserID = $currentUserID ");
+
+    $gallery_mod1_start = mysql_num_rows($gallery_mod1_mysql);
+    $gallery_mod2_start = mysql_num_rows($gallery_mod2_mysql);
+    $gallery_mod3_start = mysql_num_rows($gallery_mod3_mysql);
+    $gallery_mod4_start = mysql_num_rows($gallery_mod4_mysql);
+    $gallery_mod5_start = mysql_num_rows($gallery_mod5_mysql);
+
+    //olympics modules
+    $olympics_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '2' AND CompID = '1' AND UserID = $currentUserID ");
+    $olympics_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '2' AND CompID = '2' AND UserID = $currentUserID ");
+    $olympics_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '2' AND CompID = '3' AND UserID = $currentUserID ");
+    $olympics_mod4_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '2' AND CompID = '4' AND UserID = $currentUserID ");
+    $olympics_mod5_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '2' AND CompID = '5' AND UserID = $currentUserID ");
+    $olympics_mod6_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '2' AND CompID = '6' AND UserID = $currentUserID ");   
+
+    $olympics_mod1_start = mysql_num_rows($olympics_mod1_mysql);
+    $olympics_mod2_start = mysql_num_rows($olympics_mod2_mysql);
+    $olympics_mod3_start = mysql_num_rows($olympics_mod3_mysql);
+    $olympics_mod4_start = mysql_num_rows($olympics_mod4_mysql);
+    $olympics_mod5_start = mysql_num_rows($olympics_mod5_mysql);
+    $olympics_mod6_start = mysql_num_rows($olympics_mod6_mysql);
+
+    //council modules
+    $council_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '3' AND CompID = '1' AND UserID = $currentUserID ");
+    $council_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '3' AND CompID = '2' AND UserID = $currentUserID ");
+    $council_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '3' AND CompID = '3' AND UserID = $currentUserID ");
+
+    $council_mod1_start = mysql_num_rows($council_mod1_mysql);
+    $council_mod2_start = mysql_num_rows($council_mod2_mysql);
+    $council_mod3_start = mysql_num_rows($council_mod3_mysql);
+
+    //uq modules
+    $uq_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '7' AND CompID = '1' AND UserID = $currentUserID ");
+    $uq_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '7' AND CompID = '2' AND UserID = $currentUserID ");
+    $uq_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '7' AND CompID = '3' AND UserID = $currentUserID ");
+
+    $uq_mod1_start = mysql_num_rows($uq_mod1_mysql);
+    $uq_mod2_start = mysql_num_rows($uq_mod2_mysql);
+    $uq_mod3_start = mysql_num_rows($uq_mod3_mysql);
+
+    //ballet modules
+    $ballet_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '5' AND CompID = '1' AND UserID = $currentUserID ");
+    $ballet_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '5' AND CompID = '2' AND UserID = $currentUserID ");
+    $ballet_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '5' AND CompID = '3' AND UserID = $currentUserID ");
+
+    $ballet_mod1_start = mysql_num_rows($ballet_mod1_mysql);
+    $ballet_mod2_start = mysql_num_rows($ballet_mod2_mysql);
+    $ballet_mod3_start = mysql_num_rows($ballet_mod3_mysql);
+
+    //cycling modules
+    $cycling_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '4' AND CompID = '1' AND UserID = $currentUserID ");
+    $cycling_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '4' AND CompID = '2' AND UserID = $currentUserID ");
+    $cycling_mod3_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '4' AND CompID = '3' AND UserID = $currentUserID ");
+
+    $cycling_mod1_start = mysql_num_rows($cycling_mod1_mysql);
+    $cycling_mod2_start = mysql_num_rows($cycling_mod2_mysql);
+    $cycling_mod3_start = mysql_num_rows($cycling_mod3_mysql);
+
+    //qtac modules
+    $qtac_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '8' AND CompID = '1' AND UserID = $currentUserID ");
+    $qtac_mod2_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '8' AND CompID = '2' AND UserID = $currentUserID ");
+
+    $qtac_mod1_start = mysql_num_rows($qtac_mod1_mysql);
+    $qtac_mod2_start = mysql_num_rows($qtac_mod2_mysql);
+
+    //writers modules
+    $writers_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '6' AND CompID = '1' AND UserID = $currentUserID ");
+
+    $writers_mod1_start = mysql_num_rows($writers_mod1_mysql);
+
+    //museum modules
+    $museum_mod1_mysql = mysql_query("SELECT * FROM restorations WHERE WebsiteID = '11' AND CompID = '1' AND UserID = $currentUserID ");
+
+    $museum_mod1_start = mysql_num_rows($museum_mod1_mysql);
 
 ?>
 <!DOCTYPE html>
@@ -88,19 +188,7 @@ require("base.php");
         <!-- converting php variables to js -->
         <script>
           var cricket = <?php echo (json_encode($cricket_start)); ?>;
-          console.log("cricket var = " + cricket);
-            
-          var cricket2 = <?php echo (json_encode($cricket_start2)); ?>;
-          console.log("cricket2 (not checking for userid) var " + cricket2);
-            
-            var currentuserid = <?php echo (json_encode($currentUserID)); ?>;
-          console.log("current user id is " + currentuserid);
-            
-            var sessionusername1 = <?php echo (json_encode($SessionUsername)); ?>;
-          console.log("session username is " + sessionusername1); 
-            
-          var olympics = <?php echo (json_encode($olympics_start)); ?>;
-          
+          var olympics = <?php echo (json_encode($olympics_start)); ?>;         
           var cycling = <?php echo (json_encode($cycling_start)); ?>;
           var ballet = <?php echo (json_encode($ballet_start)); ?>;
           var writers = <?php echo (json_encode($writers_start)); ?>;
@@ -109,6 +197,51 @@ require("base.php");
           var council = <?php echo (json_encode($council_start)); ?>;
           var gallery = <?php echo (json_encode($gallery_start)); ?>;
           var museum = <?php echo (json_encode($museum_start)); ?>;
+          
+          var cricketmod1 = <?php echo (json_encode($cricket_mod1_start)); ?>;
+          var cricketmod2 = <?php echo (json_encode($cricket_mod2_start)); ?>;
+          var cricketmod3 = <?php echo (json_encode($cricket_mod3_start)); ?>;
+          var cricketmod4 = <?php echo (json_encode($cricket_mod4_start)); ?>;
+          var cricketmod5 = <?php echo (json_encode($cricket_mod5_start)); ?>;
+          var cricketmod6 = <?php echo (json_encode($cricket_mod6_start)); ?>;
+          var cricketmod7 = <?php echo (json_encode($cricket_mod7_start)); ?>;
+          var cricketmod8 = <?php echo (json_encode($cricket_mod8_start)); ?>;
+          
+          var gallerymod1 = <?php echo (json_encode($gallery_mod1_start)); ?>;
+          var gallerymod2 = <?php echo (json_encode($gallery_mod2_start)); ?>;
+          var gallerymod3 = <?php echo (json_encode($gallery_mod3_start)); ?>;
+          var gallerymod4 = <?php echo (json_encode($gallery_mod4_start)); ?>;
+          var gallerymod5 = <?php echo (json_encode($gallery_mod5_start)); ?>;
+          
+          var olympicsmod1 = <?php echo (json_encode($olympics_mod1_start)); ?>;
+          var olympicsmod2 = <?php echo (json_encode($olympics_mod2_start)); ?>;
+          var olympicsmod3 = <?php echo (json_encode($olympics_mod3_start)); ?>;
+          var olympicsmod4 = <?php echo (json_encode($olympics_mod4_start)); ?>;
+          var olympicsmod5 = <?php echo (json_encode($olympics_mod5_start)); ?>;
+          var olympicsmod6 = <?php echo (json_encode($olympics_mod6_start)); ?>;
+          
+          var councilmod1 = <?php echo (json_encode($council_mod1_start)); ?>;
+          var councilmod2 = <?php echo (json_encode($council_mod2_start)); ?>;
+          var councilmod3 = <?php echo (json_encode($council_mod3_start)); ?>;
+          
+          var uqmod1 = <?php echo (json_encode($uq_mod1_start)); ?>;
+          var uqmod2 = <?php echo (json_encode($uq_mod2_start)); ?>;
+          var uqmod3 = <?php echo (json_encode($uq_mod3_start)); ?>;
+          
+          var balletmod1 = <?php echo (json_encode($ballet_mod1_start)); ?>;
+          var balletmod2 = <?php echo (json_encode($ballet_mod2_start)); ?>;
+          var balletmod3 = <?php echo (json_encode($ballet_mod3_start)); ?>;
+          
+          var cyclingmod1 = <?php echo (json_encode($cycling_mod1_start)); ?>;
+          var cyclingmod2 = <?php echo (json_encode($cycling_mod2_start)); ?>;
+          var cyclingmod3 = <?php echo (json_encode($cycling_mod3_start)); ?>;
+          
+          var qtacmod1 = <?php echo (json_encode($qtac_mod1_start)); ?>;
+          var qtacmod2 = <?php echo (json_encode($qtac_mod2_start)); ?>;
+          
+          var writersmod1 = <?php echo (json_encode($writers_mod1_start)); ?>;
+          
+          var museummod1 = <?php echo (json_encode($museum_mod1_start)); ?>;
 
         </script>
     
